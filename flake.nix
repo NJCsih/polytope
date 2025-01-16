6d3abd079df4d16c5ebadca45e9e1c3afecb8c16
NJCsih <29319746+NJCsih@users.noreply.github.com>
2025-01-16 14:21:24 -0700

    Updates

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0-rc1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       #inputs.nixpkgs.follows = "nixpkgs-stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
