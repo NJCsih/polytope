3b18b4538a588d4081b132c9a4563218c1aa5a8f
NJCsih <29319746+NJCsih@users.noreply.github.com>
2025-09-24 22:43:21 -0600

    chore: flake updates

--- flake.nix
+++ flake.nix
@@ -25,6 +25,11 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
